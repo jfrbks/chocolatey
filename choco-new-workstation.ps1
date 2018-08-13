b3b55b3ba3a25b77d4be8618bca1d29bdae552fd
jfrbks <41306593+jfrbks@users.noreply.github.com>
2018-08-13 13:36:50 -0500

    Initial commit

---
+++ choco-new-workstation.ps1
@@ -0,0 +1,29 @@
+choco install 7zip -y
+choco install adobereader -y
+choco install audacity -y
+choco install deluge -y
+choco install f.lux -y
+choco install filezilla -y
+choco install firefox -y
+choco install malwarebytes -y
+choco install notepadplusplus  -y
+choco install office365proplus 
+choco install paint.net -y
+choco install putty.install -y
+choco install python -y
+choco install qbittorrent -y
+choco install rdcman -y
+
+choco install rufus -y
+# Create a Shortcut
+$TargetFile = "C:\ProgramData\chocolatey\lib\rufus\tools\rufus.exe"
+$ShortcutFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Rufus.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
+choco install rvtools -y
+choco install vlc -y
+choco install win32diskimager -y
+choco install yakyak --ignore-checksums -y
